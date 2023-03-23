29ec4fedb56d7b27d87e868a50ca4ae19768c098
Raja <kelaya5@gmail.com>
2023-03-23 20:02:53 +0530

    commit

---
+++ basics/Manager8.java
@@ -0,0 +1,27 @@
+public class Manager8 {
+    /*
+     * logical operators
+     * and(&&)
+     * or(||)
+     * not(!)
+     * 
+     */
+    public static void main(String[] args) {
+        /*  ==    10 == 10 (true)
+            !=    20 !=  10  ()
+         *  <
+         *  >
+            <=
+            >=
+            */ 
+
+          //  System.out.println( (10 >= 3)  (10==20));
+
+          if(10==10){
+            System.out.println("from if");
+          }
+          else{
+            System.out.println("from else");
+          }
+    }    
+}
