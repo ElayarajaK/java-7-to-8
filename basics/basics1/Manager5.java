7b48a33d0e75cfd409b53ec4af6b044c40cb8669
Raja <kelaya5@gmail.com>
2023-03-30 20:10:42 +0530

    commit

---
+++ basics/basics1/Manager5.java
@@ -0,0 +1,16 @@
+public class Manager5 {
+    public static void main(String[] args) {
+       /* 
+        if(true){
+
+            break;
+        } */
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i==0);
+    }
+    
+}
---
+++ basics/basics1/Manager6.java
@@ -0,0 +1,26 @@
+
+
+public class Manager6 
+{
+    public static void main(String[] args) 
+    {
+        int i=10;
+        String name="Raja";
+
+        boolean b1=true;
+
+        int[]values={10,20,30,40,50,60,70};//array declaration
+        /*System.out.println(values[0]);
+        System.out.println(values[2]);
+        System.out.println(values[4]);
+        System.out.println(values[6]);
+        System.out.println(values[7]);*/
+
+        for(int k=0;k<values.length;k++){
+            System.out.println(values[k]);
+        }
+
+        //System.out.println(values.length);//7
+    }   
+    
+}
