98d6419c7319fb972bf1fd43f47100bfeeb7f0e1
Raja <kelaya5@gmail.com>
2023-04-05 20:13:39 +0530

    commit

---
+++ basics/basics1/Manager8.java
@@ -0,0 +1,23 @@
+public class Manager8 {
+
+    public static void main(String[] args) {
+        int arr[]={10,20,30,60,10,30};
+        int max=0;
+        System.out.println(arr.length);       
+         for(int i=0;i<arr.length;i++)
+         {
+            if(arr[i] > max)
+            {
+                max=arr[i];
+            }
+         }
+         System.out.println(max);      
+ 
+
+
+
+
+
+    }
+    
+}
